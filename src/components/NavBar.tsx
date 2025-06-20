19e47ee8b1fdbcabf30c5b5bddcc680a7dec0498
DevSaLLein <devzac.andrade@gmail.com>
2025-06-20 15:46:38 -0300

    fix: rename *.css to *.module.css from navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,20 @@
-import './NavBar.css';
+import Styles from "./NavBar.module.css";
 
 export const NavBar = () => {
+
     return (
-        <nav className="navbar">
-        <div className="navbar__logo">
-            <img src="/logo.png" alt="Logo" />
-        </div>
-        <ul className="navbar__links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
+
+        <nav className={Styles.navbar}>
+            <div className={Styles.navbar__logo}>
+                <img src="/logo.png" alt="Logo" />
+            </div>
+            
+            <ul className={Styles.navbar__links}>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
         </nav>
     );
-    }
+}
