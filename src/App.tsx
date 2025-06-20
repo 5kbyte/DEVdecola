23f8d2b793a4e953821f10d1be73b36ae027b621
migueljuan <miguel.freitas0125@jala.univeristy>
2025-06-20 14:33:41 -0300

    criação de código para navbar não definitava

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import {NavBar} from './components/NavBar'
 
 function App() {
 
   return (
     <>
+      <NavBar />
+      <div className="app-content">
+        <h1>Welcome to DEVdecola</h1>
+        <p>Your one-stop solution for all things development!</p>
+      </div>
     </>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar">
+        <div className="navbar__logo">
+            <img src="/logo.png" alt="Logo" />
+        </div>
+        <ul className="navbar__links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        </nav>
+    );
+    }
