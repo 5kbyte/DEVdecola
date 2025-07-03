8313f4003033c09a9841c23204754c5054d53c5e
viniciusfialhu <Vinicius.Fialho0125@jala.university>
2025-07-03 12:35:24 -0300

    fix: Ajustado o ambiente e testado os endpoint realizados

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsuarioModule } from './modules/usuario/usuario.module';
 import { EmpresaModule } from './modules/empresa/empresa.module';
 import { DesenvolvedorModule } from './modules/desenvolvedor/desenvolvedor.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [UsuarioModule, EmpresaModule, DesenvolvedorModule, PrismaModule, AuthModule],
