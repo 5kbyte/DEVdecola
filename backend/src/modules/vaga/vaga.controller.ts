7f278d2af41f4f8e1e02fa3901fedb07c7306982
PedroKeita <pedrolucas300805@gmail.com>
2025-07-07 17:45:38 -0300

    docs(swagger): initialization of swagger

--- backend/src/modules/vaga/vaga.controller.ts
+++ backend/src/modules/vaga/vaga.controller.ts
@@ -1,12 +1,15 @@
 import { Controller,  Post, Body, Get } from '@nestjs/common';
 import { VagaService } from './vaga.service';
 import { CreateVagaDto } from './dto/create-vaga.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('vaga')
 export class VagaController {
   constructor(private readonly vagaService: VagaService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Cria uma nova vaga' })
+  @ApiBearerAuth()
   create(@Body() createVagaDto: CreateVagaDto) {
     return this.vagaService.createVaga(createVagaDto);
   }
--- backend/src/modules/vaga/vaga.module.ts
+++ backend/src/modules/vaga/vaga.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VagaService } from './vaga.service';
 import { VagaController } from './vaga.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [VagaController],
   providers: [VagaService],
 })
